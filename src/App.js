c5b00d4d3f6264d6c8825ce87101eec8475f5f61
Patryk <patrykmigala0@gmail.com>
2025-04-17 17:52:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { act, useState } from 'react';
+import { useState } from 'react';
 import './index.css';
 
 const initialItems = [
@@ -21,12 +21,14 @@ const initialItems = [
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
     },
     {
+
         name: "Multimetr 500V/1500A",
         cost: 192.89,
         image: 'https://i.pinimg.com/736x/64/e5/d2/64e5d2a8c2d27cee1df28b6afc2f8766.jpg',
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
     },
     {
+
         name: "Glock-19 Gen 5 kal. 9x19mm",
         cost: 1029,
         image: 'https://i.pinimg.com/736x/5e/1c/73/5e1c73dae097396ee8afe4b40cc77c33.jpg',
@@ -35,71 +37,110 @@ const initialItems = [
 ]
 
 export default function App() {
-    const items = initialItems;
+    return (
+        <>
+            <Header>Shop Market</Header>
+            <Container />
+        </>
+    )
+}
+
+function Container() {
+    const [showShoppingCard, setShowShoppingCard] = useState(false);
+    const [shoppingItems, setShoppingItems] = useState([]);
     const [showDescription, setShowDescription] = useState(false);
-    const [showShoppingCard, setShowShoppingCard] = useState(true);
     const [inputItem, setInputItem] = useState('');
     const [product, setProduct] = useState({});
-    //const [countProduct, setCountProduct] = useState(1);
+    const [money, setMoney] = useState(3140);
 
-    const [shoppingItems, setShoppingItems] = useState([
-        {
-            name: "little plushie",
-            cost: 29.99,
-            image: 'https://i.pinimg.com/736x/2b/27/50/2b275054775019ef566f5aa4d3ba2330.jpg'
-        }, {
-            name: "Glock-19 Gen 5 kal. 9x19mm",
-            cost: 1029,
-            image: 'https://i.pinimg.com/736x/5e/1c/73/5e1c73dae097396ee8afe4b40cc77c33.jpg'
+    function handleAddItem(item) {
+        setShoppingItems([...shoppingItems, { ...item, id: Date.now() }]);
+        setShowShoppingCard(true)
+    }
 
-        }]);
+    function handleDeleteItem(item) {
+        setShoppingItems(shoppingItems.filter((el) => el.id !== item.id));
+    }
 
-    function handleAddToCard(item){
-        /*shoppingItems.map(el => {
-            if(el.name === item.name)  
-        })*/
-        setShoppingItems([...shoppingItems, item]);
-    } 
+    function handleClear(arr) {
+        const amount = arr.map(el => el.cost);
+        const cost = amount.length > 0 ? amount.reduce((acc, el) => acc + el).toFixed(2) : 0;
 
+        setMoney(money >= cost ? money - cost : money)
+        setShoppingItems(money >= cost ? [] : arr);
+
+    }
 
     return (
         <div className="container">
-            <div className="list-items">
-                <Search 
-                    items={items} 
-                    handleInput={setInputItem} 
+            <LeftSide >
+                <Search
+                    onInputItem={setInputItem}
                     inputItem={inputItem} />
-                <ListItems 
-                    items={items} 
-                    inputItem={inputItem} 
-                    onShowDescription={setShowDescription} 
-                    showDescription={showDescription} 
-                    setProduct={setProduct} 
-                    onAddToCard={handleAddToCard}/>
-            </div>
-            {showDescription && <ItemDescription product={product} onAddToCard={handleAddToCard} />}
-            <div className="right-side">
-                <NumItems shoppingItems={shoppingItems} onShowShoppingCard={setShowShoppingCard} />
-                {showShoppingCard ? <ShoppingCard shoppingItems={shoppingItems} /> : ""}
-            </div>
+                <ListItems
+                    inputItem={inputItem}
+                    onShowDescription={setShowDescription}
+                    showDescription={showDescription}
+                    setProduct={setProduct}
+                    onAddItem={handleAddItem} />
+            </LeftSide>
+            {showDescription && <ItemDescription product={product} onAddItem={handleAddItem} />}
+            <RightSide >
+                <NumItems
+                    shoppingItems={shoppingItems}
+                    onShowShoppingCard={setShowShoppingCard} money={money}/>
+                {showShoppingCard ? <ShoppingCard
+                    
+                    shoppingItems={shoppingItems}
+                    onDeleteITem={handleDeleteItem}
+                    onClear={handleClear} /> : ""}
+            </RightSide>
         </div>
     )
 }
 
-function Search({ inputItem, handleInput }) {
+function LeftSide({ children }) {
+    return (
+        <>
+            <div className="list-items">
+                {children}
+            </div>
+        </>
+    )
+}
 
+
+function RightSide({ children }) {
+    return (
+        <div className="right-side">
+            {children}
+        </div>
+    )
+}
+
+
+function Header({ children }) {
+    return (
+        <header>
+            <h1>{children}</h1>
+        </header>
+    )
+}
+
+function Search({ inputItem, onInputItem }) {
     return (
         <div>
             <div className="search">
                 <span>🔍</span>
-                <input type='text' placeholder="search..." value={inputItem} onInput={e => handleInput(e.target.value)} />
-                <span onClick={() => handleInput("")}>❌</span>
+                <input type='text' placeholder="search..." value={inputItem} onInput={e => onInputItem(e.target.value)} />
+                <span onClick={() => onInputItem("")}>❌</span>
             </div>
         </div>
     )
 }
 
-function ListItems({ items, inputItem, onShowDescription, showDescription, setProduct,onAddToCard }) {
+function ListItems({ inputItem, onShowDescription, showDescription, setProduct, onAddItem }) {
+    const items = initialItems;
     const [itemClicked, setItemClicked] = useState(null);
     return (
         <div className="items">
@@ -113,16 +154,16 @@ function ListItems({ items, inputItem, onShowDescription, showDescription, setPr
                     setItemClicked={setItemClicked}
                     onShowDescription={onShowDescription}
                     showDescription={showDescription}
-                    setProduct={setProduct} 
-                    onAddToCard={onAddToCard}
-                    />
+                    setProduct={setProduct}
+                    onAddItem={onAddItem}
+                />
                 )}
             </ul>
         </div>
     )
 }
 
-function Item({ item, inputItem, itemClicked, setItemClicked, num, onShowDescription, setProduct, onAddToCard }) {
+function Item({ item, inputItem, itemClicked, setItemClicked, num, onShowDescription, setProduct, onAddItem }) {
     const opened = num === itemClicked;
 
     function handlerItemClicked() {
@@ -135,7 +176,7 @@ function Item({ item, inputItem, itemClicked, setItemClicked, num, onShowDescrip
         <>
             {
                 inputItem === "" || item.name.toLowerCase().includes(inputItem.toLowerCase())
-                    ? 
+                    ?
                     <li className={opened ? "item-hovered" : ""}>
                         <ul className="item">
                             <li onClick={handlerItemClicked} >
@@ -148,7 +189,7 @@ function Item({ item, inputItem, itemClicked, setItemClicked, num, onShowDescrip
                                 </div>
                             </li>
                         </ul>
-                        <span className="add" onClick={()=>onAddToCard(item)} >+</span>
+                        <span className="add" onClick={() => onAddItem(item)} >+</span>
                     </li>
                     : ""
             }
@@ -157,7 +198,7 @@ function Item({ item, inputItem, itemClicked, setItemClicked, num, onShowDescrip
     )
 }
 
-function ItemDescription({ product, onAddToCard }) {
+function ItemDescription({ product, onAddItem }) {
     return (
         <div className="item-description">
             <img src={product.image} alt={product.name} />
@@ -167,80 +208,66 @@ function ItemDescription({ product, onAddToCard }) {
                 <p>
                     {product.description}
                 </p>
-                <button onClick={()=>onAddToCard(product)}>Add to card</button>
+                <button onClick={() => onAddItem(product)}>Add to card</button>
             </div>
         </div>
     )
 }
 
-function NumItems({shoppingItems, onShowShoppingCard}) {
+function NumItems({ shoppingItems, onShowShoppingCard, money }) {
     const numItems = shoppingItems.length
     return (
-        <div className="shop-num" onClick={()=>onShowShoppingCard(value=> !value)}>
-            <span className="koszyk">🛒</span>
-            <div className="num-items"><span>{numItems}</span></div>
-        </div>
+        <>  <span className="pocket">Your pocket: {money.toFixed(2)} zł</span>
+            <div className="shop-num" onClick={() => onShowShoppingCard(value => !value)}>
+                <span className="koszyk">🛒</span>
+                <div className="num-items"><span>{numItems}</span></div>
+            </div>
+        </>
     )
 }
 
-function ShoppingCard({ shoppingItems }) {
-    const [totalCost, setTotalCost] = useState(null)
-    
+function ShoppingCard({ shoppingItems, onDeleteITem, onClear }) {
+    const amount = shoppingItems.map(el => el.cost);
+    const cost = amount.length > 0 ? "Cost: " + amount.reduce((acc, el) => acc + el).toFixed(2) + " zł" : "Add items"
+
+
     return (
-        <div className="shopping-card">
-            <div>
-                <h3>Shopping card</h3>
-                <hr />
-                <ul>
-                    {shoppingItems.map((item, i) => <ShoppingItem num={i} 
-                                                        item={item} 
-                                                        key={item.image} 
-                                                        shoppingItems={shoppingItems}
-                                                        setTotalCost={setTotalCost}
-                                                        />)}
-                </ul>
-            </div>
-            <hr />
+        <>
 
-            <div className="buy">
-                <span>Cost: {totalCost} zł</span>
-                <button>Buy</button>
+            <div className="shopping-card">
+                <div>
+                    <h3>Shopping card</h3>
+                    <hr />
+                    <ul>
+                        {shoppingItems.map((item, i) => <ShoppingItem
+                            item={item}
+                            key={`${item.name + i}`}
+                            onDeleteItem={onDeleteITem}
+                        />)}
+                    </ul>
+                </div>
+                <hr />
+
+                <div className="buy">
+                    <span>{cost}</span>
+                    {amount.length > 0 ? <button onClick={() => onClear(shoppingItems)}>Buy</button> : ""} 
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
-function ShoppingItem({item, shoppingItems, setTotalCost}) {
+function ShoppingItem({ item, onDeleteItem }) {
 
-    const [amountItems, setAmountItems] = useState(1);
-    let  itemsCost = item.cost * amountItems;
-
-    function handleAdd(){
-        setAmountItems(amountItems + 1);
-        setTotalCost(itemsCost)
-
-        }
-
-    function handleSubstract(){
-        amountItems > 1 && setAmountItems(amountItems - 1);
-        setTotalCost(itemsCost)
-    }
-
-    
-    //shoppingItems.map(el=> el.name === item.name ?  : "")
     return (
         <li>
             <div>
                 <img src={item.image} alt={item.name} />
                 <h4>{item.name}</h4>
-
             </div>
-            <div>
-                <button onClick={handleSubstract}>-</button>
-                <input disabled type="text" value={amountItems} onChange={(e)=>setAmountItems(e.target.value)} />
-                <button onClick={handleAdd}>+</button>
-                <span>{(itemsCost).toFixed(2)} zł</span>
-                <span className="delete-item">❌</span>
+            <div >
+                <span>{(item.cost).toFixed(2)} zł</span>
+                <span className="delete-item" onClick={() => onDeleteItem(item)}>❌</span>
             </div>
         </li>
     )
